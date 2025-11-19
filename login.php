d0bebbc56e4343346825b97ac5a4bde603d25581
Muhammad Farhan <157960532+mfarhancode@users.noreply.github.com>
2025-11-19 18:37:23 +0700

    Wrote code for login.php and Add login functionality with session management (Farhan)

---
+++ login.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<body>
+ <h1>Login Page!</h1>
+ <form method="post">
+    <label>Username:</label>
+    <input type="text" name="username" required>
+    <br>
+    <label>Password:</label>
+    <input type="password" name="password">
+    <br>
+    <input type="submit" value="Submit">
+ </form>    
+</body>
+</html>
+
+<?php
+
+session_start();
+require "connection.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $username = htmlspecialchars($_POST['username']) ?? "";
+  $password = htmlspecialchars($_POST['password']) ?? "";
+
+  $q = "SELECT * FROM bst_user WHERE username = '$username'";
+  $data = $conn->query($q);
+  if ($data->num_rows == 0) {
+        echo "<p style='color:red'>No data found with this username.</p>";
+    } else {
+      $user_data = mysqli_fetch_assoc($data);
+      // echo $password;
+      if (password_verify($password, $user_data['password'])) {
+            session_regenerate_id(true);
+            $_SESSION['login'] = true;
+            $_SESSION['name'] = $user_data['name'];
+            $_SESSION['username'] = $user_data['username'];
+            $_SESSION['level'] = $user_data['level'];
+
+            if ($user_data['level'] == 'seller'){
+            header("Location: seller/seller_dashboard.php");
+            exit;
+            }elseif($user_data['level'] == 'admin'){
+            header("Location: admin/admin_dashboard.php");
+            exit;
+            }elseif($user_data['level'] == 'buyer'){
+            header("Location: buyer/products.php");
+            exit;
+            }
+            
+      } else {
+                echo "<p style='color:red'>Wrong Password</p>";
+      }
+        // $name = $user_data['name'];
+        // echo "<p>Welcome $name</p>";
+    }
+}
+?>
