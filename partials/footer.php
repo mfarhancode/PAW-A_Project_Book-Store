bcbc8453bb3d0401460f5a92202e8fd74d7e9047
Muhammad Farhan <157960532+mfarhancode@users.noreply.github.com>
2025-11-30 12:19:01 +0700

    create header and footer

---
+++ partials/footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <hr>
+    <p>© 2025 Book Store</p>
+</footer>
+
+<style>
+footer {
+    background: #eee;
+    padding: 10px;
+    text-align: center;
+
+    bottom: 0;
+    width: 100%;
+}
+</style>
---
+++ partials/header.php
@@ -0,0 +1,25 @@
+<div class="header">
+    <div class="left">Book Store</div>
+
+    <div class="right">
+        <a href="/PAW/PAW-A_Project_Book-Store-final/logout.php">Logout</a>
+    </div>
+</div>
+
+<style>
+.header {
+    background:#3A7BD5;
+    color: white;
+    padding: 12px 20px;
+    display: flex;
+    justify-content: space-between;
+}
+
+.header a {
+    color: white;
+    text-decoration: none;
+}
+.header a:hover {
+    text-decoration: underline;
+}
+</style>
