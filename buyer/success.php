c5972ed8f1a71577a5b3b138b4ea834332490c4e
nabilahaniffebriyanti <nabilafebriyanti526@gmail.com>
2025-11-30 12:39:19 +0700

    Update success.php

--- buyer/success.php
+++ buyer/success.php
@@ -1,133 +1,91 @@
 <?php
-
 session_start();
 
-
-
 if (!($_SESSION['login']) or $_SESSION['level'] != 'buyer') {
-
     header("Location: ../login.php");
-
     exit;
-
 }
-
-
 
 require "../connection.php";
 
-
-
 $payment_id = intval($_GET['payment_id']);
-
 $buyer_id = $_SESSION['id'];
-
 $name = $_SESSION['name'];
 
-
-
 // get payment info
-
 $q = "SELECT *
-
       FROM bst_payment_detail
-
       WHERE payment_id = '$payment_id' AND buyer_id = '$buyer_id'";
 
-
-
 $res = mysqli_query($conn, $q);
-
 $pay = mysqli_fetch_assoc($res);
 
-
-
 if (!$pay) {
-
     echo "Payment not found";
-
     exit;
-
 }
 
-
-
-// get books info
-
+// get books info (LEFT JOIN so title still appears even if deleted later)
 $q2 = "SELECT s.qty, s.price_at_sale, s.ISBN, b.judul
-
        FROM bst_sold_books s
-
-       JOIN bst_books b ON s.ISBN = b.ISBN
-
+       LEFT JOIN bst_books b ON s.ISBN = b.ISBN
        WHERE s.payment_id = '$payment_id'";
 
-
-
 $books = mysqli_query($conn, $q2);
-
 ?>
 
-<h2>Payment Success</h2>
+<link rel="stylesheet" href="../assets/success.css">
 
-<h3>Receipt</h3>
+<div class="container">
+    <h2>Payment Success</h2>
 
-<p>Name: <?php echo $name; ?></p>
+    <h3>Receipt</h3>
 
-<p>Payment ID: <?php echo $payment_id; ?></p>
+    <p><b>Name:</b> <?php echo $name; ?></p>
+    <p><b>Payment ID:</b> <?php echo $payment_id; ?></p>
+    <p><b>Date:</b> <?php echo $pay['timestamp']; ?></p>
+    <p><b>Total Price:</b> <?php echo $pay['total_harga']; ?></p>
+    <p><b>Delivery Type:</b> <?php echo $pay['delivery_type']; ?></p>
+    <p><b>Payment Method:</b> <?php echo $pay['payment_method']; ?></p>
 
-<p>Date: <?php echo $pay['timestamp']; ?></p>
+    <?php if ($pay['delivery_type'] == "delivery") { ?>
+        <h3>Delivery Details</h3>
+        <p><b>Address:</b> <?php echo $pay['address']; ?></p>
+        <p><b>City:</b> <?php echo $pay['city']; ?></p>
+        <p><b>Postal Code:</b> <?php echo $pay['postal_code']; ?></p>
+        <p><b>Phone:</b> <?php echo $pay['phone']; ?></p>
+    <?php } ?>
 
-<p>Total Price: <?php echo $pay['total_harga']; ?></p>
+    <h3>Books Purchased</h3>
 
-<p>Delivery Type: <?php echo $pay['delivery_type']; ?></p>
+    <table border="1" cellpadding="5">
+        <tr>
+            <th>Title</th>
+            <th>ISBN</th>
+            <th>Qty</th>
+            <th>Price</th>
+            <th>Total</th>
+        </tr>
 
-<p>Payment Method: <?php echo $pay['payment_method']; ?></p>
+        <?php while ($b = mysqli_fetch_assoc($books)) { 
+            $title = $b['judul'] ? $b['judul'] : "(Title no longer available)";
+            $t = $b['qty'] * $b['price_at_sale'];
+        ?>
+        <tr>
+            <td><?php echo $title; ?></td>
+            <td><?php echo $b['ISBN']; ?></td>
+            <td><?php echo $b['qty']; ?></td>
+            <td><?php echo $b['price_at_sale']; ?></td>
+            <td><?php echo $t; ?></td>
+        </tr>
+        <?php } ?>
+    </table>
 
-<?php if ($pay['delivery_type'] == "delivery") { ?>
+    <br>
 
-<h3>Delivery Details</h3><p>Address: <?php echo $pay['address']; ?></p><p>City: <?php echo $pay['city']; ?></p><p>Postal Code: <?php echo $pay['postal_code']; ?></p><p>Phone: <?php echo $pay['phone']; ?></p>
+    <button onclick="window.print()" class="print_btn">Print Receipt</button>
 
+    <br><br>
 
-
-<?php } ?>
-
-<h3>Books Purchased</h3>
-
-<table border="1" cellpadding="5">
-
-    <tr>
-
-        <th>Title</th>
-
-        <th>ISBN</th>
-
-        <th>Qty</th>
-
-        <th>Price</th>
-
-        <th>Total</th>
-
-    </tr>
-
-<?php while ($b = mysqli_fetch_assoc($books)) { 
-
-    $t = $b['qty'] * $b['price_at_sale'];?><tr>
-
-    <td><?php echo $b['judul']; ?></td>
-
-    <td><?php echo $b['ISBN']; ?></td>
-
-    <td><?php echo $b['qty']; ?></td>
-
-    <td><?php echo $b['price_at_sale']; ?></td>
-
-    <td><?php echo $t; ?></td></tr><?php } ?>
-
-
-
-</table>
-
-<br>
-
-<a href="products.php">Back to Products</a>
+    <a href="products.php" class="back_btn">Back to Products</a>
+</div>
