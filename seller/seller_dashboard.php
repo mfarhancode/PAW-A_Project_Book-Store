dbeef915cee368e0e836c15c7faa06b5cb0d76a0
Muhammad Farhan <157960532+mfarhancode@users.noreply.github.com>
2025-11-30 12:33:38 +0700

    deleted some code
    Uncomment session management and dashboard links for sellers.

--- seller/seller_dashboard.php
+++ seller/seller_dashboard.php
@@ -1,24 +1,3 @@
-<?php
-// session_start();
-// if (!($_SESSION['login']) || $_SESSION['level'] != 'seller') {
-//     header("Location: ../login.php");
-//     exit;
-// }
-
-// echo "<h1>Seller Dashboard<h1>";
-// $name =  $_SESSION['name'];
-// echo "<h2>Welcome $name!</h2>";
-// echo "<hr>";
-
-// echo "<a href='report.php'>Report</a>";
-// echo "<br><br>";
-// echo "<a href='seller_books.php'>See all your books</a>";
-// echo "<br><br>";
-// echo "<a href='add_book.php'>Add books</a>";
-
-// echo "<hr>";
-// echo "<a href='../logout.php'>Logout</a>";
-?>
 
 <?php
 session_start();
