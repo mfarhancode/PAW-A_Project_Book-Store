c1cd179b7978148cdfe865d6ef41c4ab1395de37
Muhammad Farhan <157960532+mfarhancode@users.noreply.github.com>
2025-11-30 12:33:00 +0700

    Refactor seller dashboard, styling of page

--- seller/seller_dashboard.php
+++ seller/seller_dashboard.php
@@ -1,21 +1,55 @@
 <?php
+// session_start();
+// if (!($_SESSION['login']) || $_SESSION['level'] != 'seller') {
+//     header("Location: ../login.php");
+//     exit;
+// }
+
+// echo "<h1>Seller Dashboard<h1>";
+// $name =  $_SESSION['name'];
+// echo "<h2>Welcome $name!</h2>";
+// echo "<hr>";
+
+// echo "<a href='report.php'>Report</a>";
+// echo "<br><br>";
+// echo "<a href='seller_books.php'>See all your books</a>";
+// echo "<br><br>";
+// echo "<a href='add_book.php'>Add books</a>";
+
+// echo "<hr>";
+// echo "<a href='../logout.php'>Logout</a>";
+?>
+
+<?php
 session_start();
 if (!($_SESSION['login']) || $_SESSION['level'] != 'seller') {
     header("Location: ../login.php");
     exit;
 }
-
-echo "<h1>Seller Dashboard<h1>";
-$name =  $_SESSION['name'];
-echo "<h2>Welcome $name!</h2>";
-echo "<hr>";
-
-echo "<a href='report.php'>Report</a>";
-echo "<br><br>";
-echo "<a href='seller_books.php'>See all your books</a>";
-echo "<br><br>";
-echo "<a href='add_book.php'>Add books</a>";
-
-echo "<hr>";
-echo "<a href='../logout.php'>Logout</a>";
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="../assets/seller_dashboard.css">
+</head>
+<body>
+
+<?php include "../partials/header.php"; ?>
+
+<div class="dashboard-container">
+
+    <h1>Seller Dashboard</h1>
+    <h2>Welcome <?= htmlspecialchars($_SESSION['name']) ?>!</h2>
+    <hr>
+
+    
+    <a href="seller_books.php">See all your books</a><br>
+    <a href="add_book.php">Add Books</a><br>
+    <a href="report.php">Report</a><br>
+
+</div>
+
+<?php include "../partials/footer.php"; ?>
+
+</body>
+</html>
